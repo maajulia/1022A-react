c153cd2850c78bd4ecd56e5f664f362ce85b7523
maajulia <118356114+maajulia@users.noreply.github.com>
2024-03-19 10:14:25 -0400

    Box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,8 +1,17 @@
+import './Main.css'
 function Main(){
     return(
         <>
-        <main>Julia</main>
-        <h1>Meu h1</h1>
+    <main>
+<div className='estudante-box'>
+    <img className='estudante_foto' src="https://github.com/maajulia.png" alt=""/>
+    <div className='informacoes'>
+    <h3 className= "estudante_nome">Maria Julia Alves</h3>
+    <p className='estudante_descricao'> Estudante do IFMS campus Naviraí, cursando o ensino medio integrado em informática para internet. </p>
+    <a  className = 'link-git' href='https://github.com/'>GitHub</a>
+ </div>
+</div>
+     </main>
         </>
     )
 }
